bbe086e4b0a2c14258db43f892de12cae2e462f7
matei8 <matei8escu@gmail.com>
2021-01-27 17:17:15 +0200

    Prima chestie

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode.java
@@ -1,8 +1,23 @@
 package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-public class TeamCode {
+@TeleOp(name="Simple driving", group="Tele")
+public class TeamCode extends OpMode {
     DcMotor leftMotor;
     DcMotor rightMotor;
-    DcMotor front;
+    double power = 0.5;
+
+    @Override
+    public void init() {
+        leftMotor = hardwareMap.dcMotor.get("drivetrain_left_motor");
+        rightMotor = hardwareMap.dcMotor.get("drivetrain_right_motor");
+    }
+
+    @Override
+    public void loop() {
+        leftMotor.setPower(power);
+        rightMotor.setPower(power);
+    }
 }
