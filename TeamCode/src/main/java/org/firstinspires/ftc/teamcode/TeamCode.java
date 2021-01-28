7ae4ff28a239997a39f53e39c5e200a374388bc2
matei8 <matei8escu@gmail.com>
2021-01-28 12:35:04 +0200

    Reversed the motor rotation

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeamCode.java
@@ -2,6 +2,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import static com.qualcomm.robotcore.hardware.DcMotorSimple.Direction.REVERSE;
 
 @TeleOp(name="Simple driving", group="Tele")
 public class TeamCode extends OpMode {
@@ -13,6 +16,8 @@ public class TeamCode extends OpMode {
     public void init() {
         leftMotor = hardwareMap.dcMotor.get("drivetrain_left_motor");
         rightMotor = hardwareMap.dcMotor.get("drivetrain_right_motor");
+
+        rightMotor.setDirection(REVERSE);
     }
 
     @Override
