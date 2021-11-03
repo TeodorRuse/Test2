32b8b38b146030025e26e4dc64cb882fa925d623
Teo <teodor.ruse@yahoo.com>
2021-11-03 17:20:54 +0200

    NewTest4 (FirstFinal)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ringDetector.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ringDetector.java
@@ -3,7 +3,6 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
-import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.opencv.core.Core;
@@ -14,8 +13,6 @@ import org.opencv.imgproc.Imgproc;
 import org.openftc.easyopencv.OpenCvCamera;
 import org.openftc.easyopencv.OpenCvCameraFactory;
 import org.openftc.easyopencv.OpenCvCameraRotation;
-import org.openftc.easyopencv.OpenCvInternalCamera;
-import org.openftc.easyopencv.OpenCvWebcam;
 import org.openftc.easyopencv.OpenCvPipeline;
 
 @Autonomous(name = "ringDetector", group = "Tests")
